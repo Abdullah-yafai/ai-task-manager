e58f2c7cbf4022c592dd04f3e17b5c1c0cfd575c
abdullah077 <abdullahyafai077@gmail.com>
2025-10-23 02:09:42 +0500

    xors fixes issue resolve check solve

--- backend/server.js
+++ backend/server.js
@@ -28,25 +28,16 @@ const allowedOrigins = [
   "http://localhost:3000"
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
-
-// ✅ Preflight request handler (Express 5 fix)
-app.options("/*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowedOrigins.includes(req.headers.origin) ? req.headers.origin : "");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(200);
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
